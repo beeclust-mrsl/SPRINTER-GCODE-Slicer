82954d422251f14484af9adbab98d68a5d9b7c25
karpenet <kedarkarpe10@gmail.com>
2020-01-20 18:24:43 +0530

    Changes to Monochrome

--- Slicer.py
+++ Slicer.py
@@ -27,9 +27,6 @@ class ImageToGcode():
         self.increment = spread/nozzles
         self.printArea = area
         self.feedrate = feedrate
-        self.red = (0.0, 0.0, 255.0, 0.0)
-        self.green = (0.0, 255.0, 0.0, 0.0)
-        self.blue = (255.0, 0.0, 0.0, 0.0)
         self.black = (0.0, 0.0, 0.0, 0.0)
         self.offsets = offsets
         self.debug_to_terminal()
@@ -38,33 +35,25 @@ class ImageToGcode():
     def make_gcode(self):
         self.output = "M106"  # Start Fan
         nozzleFirings = [0 for x in range(0, self.img.cols)]
-        nozzleFirings = [copy.copy(nozzleFirings) for x in range(0, 4)]
         scan = range(0, self.img.rows)
         scan.reverse()
         for y in scan:
             for x in range(0, self.img.cols):
                 color = cv.Get2D(self.img, y, x)
-                if color == self.red:
-                    nozzleFirings[0][x] += 1 << y % self.nozzles
-                elif color == self.green:
-                    nozzleFirings[1][x] += 1 << y % self.nozzles
-                elif color == self.blue:
-                    nozzleFirings[2][x] += 1 << y % self.nozzles
-                elif color == self.black:
-                    nozzleFirings[3][x] += 1 << y % self.nozzles
+                if color == self.black:
+                    nozzleFirings[x] += 1 << y % self.nozzles
                 else:
                     pass
             if y % 12 == 0 and y > 0:
-                for headNumber, headVals in enumerate(nozzleFirings):
+                for headVals in nozzleFirings:
                     for column, firingVal in enumerate(headVals):
                         if firingVal:
-                            currentOffset = self.offsets[headNumber]
+                            currentOffset = self.offsets
                             self.output += "G1X"+str(self.increment*column-currentOffset[0])+"Y"+str(y/12*self.spread-currentOffset[1])+"F"+str(self.feedrate)+"\n"
                             self.output += "M400\n"
-                            self.output += "M700 P"+str(headNumber)+" S"+str(firingVal)+"\n"
+                            self.output += "M700 S"+str(firingVal)+"\n"
                 #print(str(nozzleFirings))
                 nozzleFirings = [0 for x in range(0, self.img.cols)]
-                nozzleFirings = [copy.copy(nozzleFirings) for x in range(0, 4)]
         f = open(self.outFile, 'w')
         f.write(self.output)
         f.close()
@@ -81,13 +70,7 @@ class ImageToGcode():
             rowStr = ""
             for x in range(0, self.img.cols):
                 color = cv.Get2D(self.img, y, x)
-                if color == self.red:
-                    rowStr += termcolor.colored(" ", 'white', 'on_red')
-                elif color == self.green:
-                    rowStr += termcolor.colored(" ", 'white', 'on_green')
-                elif color == self.blue:
-                    rowStr += termcolor.colored(" ", 'white', 'on_blue')
-                elif color == self.black:
+                if  color == self.black:
                     rowStr += " "
                 else:
                     rowStr += termcolor.colored(" ", 'white', 'on_white')
@@ -127,18 +110,6 @@ if __name__ == "__main__":
                         action='version',
                         version="%(prog)s 0.0.1-dev"
                         )
-    parser.add_argument("-r", "--red",
-                        default="[0, 0]",
-                        help="Head offset in millimeters. Default: %(default)s"
-                        )
-    parser.add_argument("-g", "--green",
-                        default="[30.5, 0.1]",
-                        help="Head offset in millimeters. Default: %(default)s"
-                        )
-    parser.add_argument("-b", "--blue",
-                        default="[0, 0]",
-                        help="Head offset in millimeters. Default: %(default)s"
-                        )
     parser.add_argument("-k", "--black",
                         default="[0, 0]",
                         help="Head offset in millimeters. Default: %(default)s"
@@ -150,11 +121,8 @@ if __name__ == "__main__":
         sys.exit(0)  # Exit after help display
 
     args = parser.parse_args()
-    offsets = [ast.literal_eval(args.red),
-               ast.literal_eval(args.green),
-               ast.literal_eval(args.blue),
-               ast.literal_eval(args.black)
-               ]
+    offsets = ast.literal_eval(args.black)
+               
 
     imageProcessor = ImageToGcode(img=args.input,
                                   spread=float(args.spread),
